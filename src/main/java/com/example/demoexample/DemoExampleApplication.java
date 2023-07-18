db43ffca1b6fb8e62bc53176ee7b876e207d59be
ZhegMaks <zhegmaks@gmail.com>
2023-07-19 00:05:28 +0300

    first commit

---
+++ src/main/java/com/example/demoexample/DemoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoExampleApplication.class, args);
+    }
+
+}
