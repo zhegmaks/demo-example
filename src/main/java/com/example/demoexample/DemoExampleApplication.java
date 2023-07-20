e8d235244c005df27e075e999e5179ea7aa7a04e
ZhegMaks <zhegmaks@gmail.com>
2023-07-20 20:24:42 +0300

    add get request with one single param

--- src/main/java/com/example/demoexample/DemoExampleApplication.java
+++ src/main/java/com/example/demoexample/DemoExampleApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoExampleApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoExampleApplication.class, args);
     }
--- src/main/java/com/example/demoexample/HelloWorldController.java
+++ src/main/java/com/example/demoexample/HelloWorldController.java
@@ -1,12 +1,28 @@
 package com.example.demoexample;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloWorldController {
     @RequestMapping
     public String helloWorld() {
-        return "Hello world !!!";
+        return "Hello world !!!!";
+    }
+
+    @GetMapping("/index")
+    public String index(@RequestParam("name") List<String> nameParam) {
+        return nameParam.get(0);
+    }
+
+    @PreDestroy
+    public void PreDestroy() {
+        System.out.println("bye");
     }
 }
